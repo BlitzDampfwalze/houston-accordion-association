ec4bfa7b539d7bb9ca052739ee60145178028d7a
Paul Lime <paullime@gmail.com>
2019-06-21 19:27:17 -0500

    Updates

---
+++ precache-manifest.5b743052528fda93dfe0994fee0ef114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a053f87143a30f4a5c459a324adab2e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65c0e2a4ffada5385585",
+    "url": "/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "65c0e2a4ffada5385585",
+    "url": "/static/js/main.370c08ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/houston-accordion-association/precache-manifest.7e0dcd164f14d4f9dc5a2e500c45f7f7.js"
+  "/precache-manifest.5b743052528fda93dfe0994fee0ef114.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/houston-accordion-association/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
