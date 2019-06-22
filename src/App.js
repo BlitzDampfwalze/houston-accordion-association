1942a0d408d53732fddd62cccad26c9fd9ebd148
Paul Lime <paullime@gmail.com>
2019-06-22 02:32:48 -0500

    temporary view update

--- src/App.js
+++ src/App.js
@@ -7,10 +7,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
+        <h1>
           {/* Edit <code>src/App.js</code> and save to reload. */}
           Houston Accordion Association
-        </p>
+        </h1>
+        <p>est. 1990</p>
+
+        <div>~~Meets twice a month, every month~~</div>
+        <p></p>
+        <h3>First Tuesday of the month at:</h3>
+        <div>Son Nam Restaurant, 130 Gulfgate Mall, Houston, TX 77087</div>
+        <p></p>
+        <h3>Third Tuesday of the month at:</h3>
+        <div>Pizza Inn, 1801 Mangum Rd, Houston, TX 77092</div>
+        <p></p>
+        <p>more coming very soon...</p>
+
         {/* <a
           className="App-link"
           href="https://reactjs.org"
