b51629611c43dd6224614f557299ba0d7d91f872
Paul L <blitzdampfwalze@gmail.com>
2024-09-24 16:15:19 -0500

    fix: calendar to be mobile friendly.

--- src/Calendar.js
+++ src/Calendar.js
@@ -6,12 +6,12 @@ const Calendar = () => {
   return (
     <div style={{ display: 'inline-block' }}>
       <iframe 
-        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&bgcolor=%23ffffff&src=aG91c3RvbmFjY29yZGlvbmFzc29jQGdtYWlsLmNvbQ&color=%23039BE5"
-        style={{ border: 'solid 1px #777' }} 
-        width="800" 
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&bgcolor=%23616161&mode=AGENDA&showPrint=0&showDate=0&showTitle=0&src=aG91c3RvbmFjY29yZGlvbmFzc29jQGdtYWlsLmNvbQ&color=%23039BE5"
+        style={{ border: 'solid 1px #777',
+          overflow: "hidden"
+         }} 
+        width="375" 
         height="600" 
-        frameBorder="0" 
-        scrolling="no"
         title="Google Calendar"
       ></iframe>
     </div>
